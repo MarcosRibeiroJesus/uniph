ac0bac45dff82a17821f310cdd451364b1c7db18
MarcosRibeiroJesus <mrj84@outlook.com>
2016-01-30 15:40:30 -0200

    add JUnitTest

---
+++ src/test/java/br/com/sitedoph/uniph/infraestrutura/test/PersistenciaTest.java
@@ -0,0 +1,14 @@
+package br.com.sitedoph.uniph.infraestrutura.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistenciaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
